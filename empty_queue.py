66259c10d2a5020e582fd53b622dd81f4f7d2289
vennelavr2007-lab <vennelavr2007@gmail.com>
2026-09-24 23:18:19 +0530

    Create empty_queue.py

---
+++ empty_queue.py
@@ -0,0 +1,11 @@
+queue = []
+if len(queue) == 0:
+    print("Queue is empty")
+else:
+    print("Queue is not empty")
+queue.append(10)
+print("Queue:", queue)
+
+OUTPUT:
+Queue is empty
+Queue: [10]
